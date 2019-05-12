96670ec9c889b775ced565f03abce887f5374a18
Magdalena Jankowska/Magda <magda.desirec@gmail.com>
2019-05-12 12:49:30 +0100

    NWD

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,30 @@
 package com.sda.algorytmy;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        while (a != b){
+            if (a > b){
+                a = a - b;
+            }else {
+                b = b - a;
+            }
+
+        }
+        System.out.println("NWD: " + a);
+
     }
+
+
 }
